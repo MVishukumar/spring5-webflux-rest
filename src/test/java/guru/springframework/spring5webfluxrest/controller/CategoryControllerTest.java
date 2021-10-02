6e16d9609119ff54d40389ce9f6ae825b7b5216e
MVishukumar <beingvishu@gmail.com>
2021-10-03 00:27:02 +0530

    Added Category Controller post and put.

--- src/test/java/guru/springframework/spring5webfluxrest/controller/CategoryControllerTest.java
+++ src/test/java/guru/springframework/spring5webfluxrest/controller/CategoryControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -52,5 +53,35 @@ public class CategoryControllerTest {
                 .uri("/api/v1/categories/anystring")
                 .exchange()
                 .expectBody(Category.class);
+    }
+
+    @Test
+    public void createCategory() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().build()));
+
+        Mono<Category> categoryMono = Mono.just(Category.builder().build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(categoryMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    public void updateCategory() {
+        BDDMockito.given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().build()));
+
+        Mono<Category> categoryMono = Mono.just(Category.builder().build());
+
+        webTestClient.put()
+                .uri("/api/v1/categories/anyid")
+                .body(categoryMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 }
